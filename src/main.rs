0a4ad193bc07816a0ad15f8ba91893185de2abf3
Pedro <pjg2@pdx.edu>
2023-10-09 14:47:15 -0700

    adding print board logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut board:Vec<Vec<char>> = vec![vec!['-','-'],vec![' ','-'],vec!['>','|']];
+    //println!("{:?}",board);
+    print_board(&board);
+}
+
+fn print_board(board: &Vec<Vec<char>>) {
+    for row in board {
+        for character in row {
+            print!("{}",character);
+        }
+        println!("");
+    }
+}
+
+fn step(board: &Vec<Vec<char>>) -> Result<Vec<Vec<char>>,&'static str> {
+    return Err("Err");
 }
