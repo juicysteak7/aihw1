9ade3c6b95494412ad4c737c598fd4f1ff2b8e1d
Pedro <pjg2@pdx.edu>
2023-10-09 15:08:11 -0700

    added file reader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    let mut board:Vec<Vec<char>> = vec![vec!['-','-'],vec![' ','-'],vec!['>','|']];
-    //println!("{:?}",board);
+    let mut board:Vec<Vec<char>> = Vec::new();
+    match read_bug_rush_board("bugrush.txt") {
+        Ok(result) => {
+            board = result;
+        }
+        Err(error) => {
+            println!("{}",error);
+        }
+    }
     print_board(&board);
 }
 
@@ -13,6 +23,22 @@ fn print_board(board: &Vec<Vec<char>>) {
     }
 }
 
-fn step(board: &Vec<Vec<char>>) -> Result<Vec<Vec<char>>,&'static str> {
+fn read_bug_rush_board(filename: &str) -> io::Result<Vec<Vec<char>>> {
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+
+    // Initialize the 2D vector to store the board
+    let mut board: Vec<Vec<char>> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let row: Vec<char> = line.chars().collect();
+        board.push(row);
+    }
+
+    Ok(board)
+}
+
+fn step(board: &Vec<Vec<char>>) -> Result<Vec<Vec<char>>,&'static str>{
     return Err("Err");
 }
